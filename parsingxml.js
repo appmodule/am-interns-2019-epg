40ef1d4e6649eb03b524ee448b5f4eacce7d2db2
AppModule <markobojkovic42@gmail.com>
2019-07-24 14:03:46 +0200

    test test

---
+++ parsingxml.js
@@ -0,0 +1,51 @@
+var parser = require('fast-xml-parser');
+var he = require('he');
+var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+var app = express();
+var xmlData = fs.readFileSync('/home/appmodule/Documents/EPG/public/xmlfiles/epg.xml').toString()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+ 
+var options = {
+    attributeNamePrefix : "@",
+    attrNodeName: false, //default is 'false'
+    textNodeName : "text",
+    ignoreAttributes : false,
+    ignoreNameSpace : false,
+    allowBooleanAttributes : false,
+    parseNodeValue : true,
+    parseAttributeValue : true,
+    trimValues: true,
+    cdataTagName: "__cdata", //default is 'false'
+    cdataPositionChar: "\\c",
+    localeRange: "", //To support non english character in tag/attribute values.
+    parseTrueNumberOnly: false,
+    attrValueProcessor: a => he.decode(a, {isAttributeValue: true}),//default is a=>a
+    tagValueProcessor : a => he.decode(a) //default is a=>a
+};
+
+var tObj = parser.getTraversalObj(xmlData,options);
+var jsonObj = parser.convertToJson(tObj,options);
+
+var jsonChannels = jsonObj.tv.channel;
+var jsonProgramms = jsonObj.tv.programme;
+
+fs.writeFileSync('/home/appmodule/Documents/EPG/public/jsonfiles/epg.json', JSON.stringify(jsonObj, null, 2), function(err)
+{
+    if (err){
+        return console.log(err);
+    }
+});
+
+//export const jsonCh = jsonChannels;
+//export const jsonProg = jsonProgramms;
+
+//import {jsonChannels, jsonProgramms} from 'parsingxml.js';
+
+module.exports = {jsonChannels, jsonProgramms};
