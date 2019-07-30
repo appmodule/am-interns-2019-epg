dc5fe1ccd0b1812fbb95fa3011399d579233a15f
AppModule <markobojkovic42@gmail.com>
2019-07-30 10:00:59 +0200

    test

--- index.js
+++ index.js
@@ -117,5 +117,11 @@ app.get('/event/:id/image', function (req, res){
 app.listen(3000, function (){
   console.log('Node app is running on port 3000');
 });
+  
+
+console.log("Branch test");
+
+
+
 
 
